64893173b8d108a04a1e40584c2fbd075bfde840
Eduardo <eduardomarques@outlook.com>
2019-03-21 23:31:48 -0300

    Treatment for denial of location

--- Pokemon-GO/ViewController.swift
+++ Pokemon-GO/ViewController.swift
@@ -21,4 +21,24 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
     }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if ((status != .authorizedWhenInUse) && (status != .notDetermined)){
+            //Location Alert authorization
+            let alertController = UIAlertController(title: "Permissão de localização", message: "Para que você possa caçar pokemons, precisamos da sua localização, por favor habilite.", preferredStyle: .alert)
+            
+            let actionSetting = UIAlertAction(title: "Abrir Configurações", style: .default) { (alertSettings) in
+                if let settings = NSURL(string: UIApplication.openSettingsURLString){
+                    UIApplication.shared.open(settings as URL)
+                }
+            }
+            
+            let actionCancel = UIAlertAction(title: "Cancelar", style: .default, handler: nil)
+            
+            alertController.addAction(actionSetting)
+            alertController.addAction(actionCancel)
+            
+            present(alertController, animated: true, completion: nil)
+        }
+    }
 }
