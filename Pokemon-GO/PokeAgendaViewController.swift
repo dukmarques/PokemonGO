536043a21c0f15391ddd280ab7ebd6a34fb65057
Eduardo <eduardomarques@outlook.com>
2019-03-23 02:06:46 -0300

    poke-agenda navigation

---
+++ Pokemon-GO/PokeAgendaViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PokeAgendaViewController.swift
+//  Pokemon-GO
+//
+//  Created by Eduardo on 23/03/19.
+//  Copyright © 2019 Curso IOS. All rights reserved.
+//
+
+import UIKit
+
+class PokeAgendaViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func backMap(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
